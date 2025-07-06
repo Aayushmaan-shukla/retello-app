de94569310be6db8d6af0cd0246e82cbb26734fe
Aayushmaan-shukla <aayushmaanbelle@gmail.com>
2025-07-06 06:24:04 +0530

    updated cors on uat

--- core/config.py
+++ core/config.py
@@ -49,6 +49,8 @@ class Settings(BaseSettings):
         "http://127.0.0.1:3000",
         "http://127.0.0.1:4100",
         "https://api-retello.enpointe.io",
+        "https://api-uat-retello.enpointe.io",
+        "https://api-uat-microretello.enpointe.io",
         "https://retello.enpointe.io",
         "https://retello-uat.enpointe.io"
     ]
